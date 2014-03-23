7509940718e6615ba0f761378a2668068a24e963
Serg Koshelev <ehvfyuf@gmail.com>
2014-03-23 12:09:02 +0200

    rename

--- www/js/index.js
+++ www/js/index.js
@@ -52,5 +52,5 @@ window.addEventListener("batterystatus", onBatteryStatus, false);
 
 function onBatteryStatus(info) {
     // Handle the online event
-    console.log("Level: " + info.level + " isPlugged: " + info.isPlugged);
+    alert("Level: " + info.level + " isPlugged: " + info.isPlugged);
 }
