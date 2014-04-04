1783bb448fe622796e5165bf10ad599b4e87dedc
masterofdaemon <ehvfyuf@gmail.com>
2014-04-04 22:15:35 +0300

    add push 0.0.1

--- www/js/index.js
+++ www/js/index.js
@@ -16,6 +16,7 @@
  * specific language governing permissions and limitations
  * under the License.
  */
+var pushNotification;
 var app = {
     // Application Constructor
     initialize: function() {
@@ -35,18 +36,35 @@ var app = {
     onDeviceReady: function() {
 
         
-
+        
         app.receivedEvent('deviceready');
     },
     // Update DOM on a Received Event
     receivedEvent: function(id) {
+        pushNotification = window.plugins.pushNotification;
         
-        window.addEventListener("batterystatus", onBatteryStatus, false);
-VideoPlayer.play("http://www.youtube.com/watch?v=Hqnx8cTV3Sk");
+        pushNotification.register(
+        successHandler,
+        errorHandler, {
+            "senderID":"replace_with_sender_id",
+            "ecb":"onNotificationGCM"
+        });
+        
+        function errorHandler (error) {
+    alert('error = ' + error);
+}
+        function successHandler (result) {
+    alert('result = ' + result);
+}
+        
+        //window.addEventListener("batterystatus", onBatteryStatus, false);
+//VideoPlayer.play("http://www.youtube.com/watch?v=Hqnx8cTV3Sk");
 function onBatteryStatus(info) {
     // Handle the online event
     alert("Level: " + info.level + " isPlugged: " + info.isPlugged);
 }
+  
+        
         
         var parentElement = document.getElementById(id);
         var listeningElement = parentElement.querySelector('.listening');
